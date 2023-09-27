1b6f36667514f18aaf0a9e39b9028397f698643b
GuilhermeSchneider <zaquerox544@hotmail.com>
2023-09-27 19:18:36 -0300

    main

--- hello-world-example/src/com/github/guilherme/Main.java
+++ hello-world-example/src/com/github/guilherme/Main.java
@@ -3,7 +3,9 @@ package com.github.guilherme;
 public class Main {
     public static void main(String[] args) {
         String hello = "Hello World";
+        String aloow = "Aloow";
 
         System.out.println(hello);
+        System.out.println(aloow);
     }
 }
